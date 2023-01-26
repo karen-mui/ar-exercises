7d8451d41a515c21c00dfa466321b45b78c3e95b
Karen Mui <karen_mui@hotmail.com>
2023-01-26 22:09:58 +0000

    added validation and user input

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,28 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+
+class Employee < ActiveRecord::Base
+  belongs_to :store
+  validates :first_name, :last_name, :store_id, presence: true
+  validates :hourly_rate, numericality: { only_integer: true, greater_than_or_equal_to: 40, less_than_or_equal_to: 200 }
+end
+
+class Store < ActiveRecord::Base
+  has_many :employees
+  validates :name, length: { minimum: 3 }
+  validates :annual_revenue, numericality: { only_integer: true, greater_than_or_equal_to: 0}
+  validate :must_carry_apparel
+
+  def must_carry_apprel
+    if mens_apparel == false && womens_apparel == false
+      errors.add(:mens_apparel, "must carry at least one of men's or women's apparel")
+    end
+  end
+ 
+end
+
+print 'Provide a name for a new store > '
+answer = gets.chomp.to_s
+
+new_store = Store.create(name: answer)
