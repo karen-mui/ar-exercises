615de4b11b06e1fe764aff51cd427d842d79b49b
Karen Mui <karen_mui@hotmail.com>
2023-01-26 21:18:02 +0000

    created a one to many association between the two classes, added employees

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,10 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Karen", last_name: "Mui", hourly_rate: 60)
+@store1.employees.create(first_name: "Bob", last_name: "Jim", hourly_rate: 25)
+@store2.employees.create(first_name: "Amy", last_name: "Saad", hourly_rate: 141)
+@store2.employees.create(first_name: "Foo", last_name: "Bar", hourly_rate: 33)
+@store2.employees.create(first_name: "Pika", last_name: "Chu", hourly_rate: 15)
